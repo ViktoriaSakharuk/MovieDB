fba000262ced6ea397742dfe79ace28039af68b2
ViktoriaSakharuk <saharukvika@gmail.com>
2020-11-01 15:11:36 +0200

    loop, if-statement

--- script.js
+++ script.js
@@ -1,19 +1,51 @@
-const numberOfFilms = prompt("How many movies have you seen?", "");
+let numberOfFilms;
 
-const firstQuestion = prompt("What is the last movie you watched?", "");
-const secondQuestion = prompt("Rate this movie, please", "");
+while (numberOfFilms == null || numberOfFilms.length==0) {
+    numberOfFilms = prompt("How many movies have you seen?", "");
+   
+}
 
-const personalMovieDB = {
-count : numberOfFilms,
-movies : {
-    [firstQuestion] : secondQuestion
-},
-actors : {},
-genres: [],
-privat: false
+if (numberOfFilms<10) {
+    alert ('Quite a few');
+}
+else if(numberOfFilms>=10 && numberOfFilms<=30) {
+    alert ('So good!');
+} 
+else if(numberOfFilms>30) {
+    alert ('Fantastic!');
+}
+else {
+    alert('Error!');
+}
+
+let personalMovieDB = {
+    count : numberOfFilms,
+    movies : {},
+    actors : {},
+    genres: [],
+    privat: false
 };
 
+for (let i=0; i<2; i++) {
+    let a;
+    let b;
+   
+     while (a == null || a.length==0 || a.length>50) {
+        a = prompt("What is the last movie you watched?", ""); 
+    }
+
+    while (b == null || b.length==0 ) {
+        b = prompt("Rate this movie, please", ""); 
+    }
+
+    personalMovieDB.movies[a]=b;
+
+}
+
 console.log(personalMovieDB);
+
+
+
 
 
 
