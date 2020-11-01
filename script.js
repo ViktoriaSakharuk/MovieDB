a3ba98389cdaf77319569da7b72dd96adbc0d879
ViktoriaSakharuk <saharukvika@gmail.com>
2020-11-01 16:25:07 +0200

    function

--- script.js
+++ script.js
@@ -1,22 +1,31 @@
 let numberOfFilms;
 
-while (numberOfFilms == null || numberOfFilms.length==0) {
-    numberOfFilms = prompt("How many movies have you seen?", "");
-   
+function start() {
+    numberOfFilms = +prompt("How many movies have you seen?", "");
+
+    while (numberOfFilms == null || numberOfFilms == "" || isNaN(numberOfFilms)) {
+        numberOfFilms = +prompt("How many movies have you seen?", "");
+    }
 }
 
-if (numberOfFilms<10) {
-    alert ('Quite a few');
+start(); 
+
+function detectPersonalLevel() {
+    if (numberOfFilms<10) {
+        alert ('Quite a few');
+    }
+    else if(numberOfFilms>=10 && numberOfFilms<=30) {
+        alert ('So good!');
+    } 
+    else if(numberOfFilms>30) {
+        alert ('Fantastic!');
+    }
+    else {
+        alert('Error!');
+    }
 }
-else if(numberOfFilms>=10 && numberOfFilms<=30) {
-    alert ('So good!');
-} 
-else if(numberOfFilms>30) {
-    alert ('Fantastic!');
-}
-else {
-    alert('Error!');
-}
+
+detectPersonalLevel();
 
 let personalMovieDB = {
     count : numberOfFilms,
@@ -26,21 +35,44 @@ let personalMovieDB = {
     privat: false
 };
 
-for (let i=0; i<2; i++) {
-    let a;
-    let b;
-   
-     while (a == null || a.length==0 || a.length>50) {
-        a = prompt("What is the last movie you watched?", ""); 
+function showMyDB() {
+    if (personalMovieDB.privat == false) {
+        console.log(personalMovieDB);
     }
-
-    while (b == null || b.length==0 ) {
-        b = prompt("Rate this movie, please", ""); 
-    }
-
-    personalMovieDB.movies[a]=b;
-
 }
+
+showMyDB();
+
+function writeYourGenres(){
+    for ( let i=0; i<=2; i++) {
+        personalMovieDB.genres[i] = prompt("Your favorite movie genre number " + (++i), "");
+        i--;
+    }
+} 
+
+writeYourGenres();
+
+function rememberMyFilms() {
+    for (let i=0; i<2; i++) {
+        let a;
+        let b;
+       
+         while (a == null || a.length==0 || a.length>50) {
+            a = prompt("What is the last movie you watched?", ""); 
+        }
+    
+        while (b == null || b.length==0 ) {
+            b = prompt("Rate this movie, please", ""); 
+        }
+            personalMovieDB.movies[a]=b;
+    }
+}
+
+rememberMyFilms();
+
+
+
+
 
 console.log(personalMovieDB);
 
